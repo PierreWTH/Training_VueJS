ea42dc618ef2a9b22c3b96b8aa5db586e1b205d9
PierreWTH <wietrichpierre@gmail.com>
2024-01-26 23:19:42 +0100

    limit add product to bag to one article

--- Project 5 - Shopping Bag/src/views/HomePage.vue
+++ Project 5 - Shopping Bag/src/views/HomePage.vue
@@ -7,10 +7,11 @@
         <div class="product-image" :style="{backgroundImage: 'url(' + product.image + ')'}"></div>
         <h4>{{product.title}}</h4>
         <p class="price">{{product.price.toFixed(2)}}$</p>
-        <button @click="addToBag(product)">Add to bag</button>
+        <button v-if="!isInBag(product)" @click="addToBag(product)">Add to bag</button>
+        <button v-else class="remove" @click="delete(product)">Remove from bag</button>
+
       </div>
     </div>
-    {{ productsInBag.length }}
   </div>
 </template>
 
@@ -34,6 +35,9 @@ export default {
    addToBag(product) {
     product.quantity = 1;
     this.$store.dispatch('addToBag', product);
+   },
+   isInBag(product) {
+    return this.productsInBag.find(item => item.id == product.id)
    }
   }
 }
