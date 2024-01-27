27927b7dd86207ff7820eab7be4b7b788768524d
PierreWTH <wietrichpierre@gmail.com>
2024-01-27 16:20:08 +0100

    add shopping bag

--- Project 5 - Shopping Bag/src/App.vue
+++ Project 5 - Shopping Bag/src/App.vue
@@ -12,6 +12,8 @@ import { mapState } from 'vuex';
   export default {
     created() {
       this.$store.dispatch('loadProducts')
+      this.$store.dispatch('loadBag')
+
     },
     computed: mapState([
       'productsInBag'
